f972758c2b27e3af949d264da7c49e7631199295
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-02-05 15:52:19 -0300

    added length, clear, keys, remove functions

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -4,11 +4,12 @@ class HashMap
     @loadfactor = load_factor
     @capacity = capacity
     @buckets = Array.new(@capacity) {[]}
+    @count = 0
   end
   def hash(key)
     hash_code = 0
-    # prime_number = 104729 * 350381
-    prime_number = 31
+    prime_number = 104729 * 350381
+    # prime_number = 31
     key.each_char { |char| hash_code =  prime_number * hash_code + char.ord}
     hash_code
   end
@@ -17,6 +18,7 @@ class HashMap
      bucket = @buckets[index]
      if bucket.empty?
       bucket << [key,value]
+      @count += 1
      else
       bucket[0][1] = value
      end
@@ -25,6 +27,43 @@ class HashMap
     index = hash(key) % @capacity
     bucket = @buckets[index]
     bucket[0][1]
+  end
+  def has?(key)
+    index = hash(key) % @capacity
+    bucket = @buckets[index]
+    p bucket
+    bucket.empty? ? false : true
+  end
+  def remove(key)
+    index = hash(key) % @capacity
+    bucket = @buckets[index]
+    if has?(key)
+      del1 = bucket[0][0]
+      del2 = bucket[0][1]
+      puts "Deleted key: #{del1}, value: #{del2}. "
+      bucket[0].pop
+      bucket[0].pop
+      # del1, del2
+    else
+      puts "Key not found"
+    end
+    bucket.clear
+    @count -= 1
+  end
+  def length 
+    @count
+  end
+  def clear
+    initialize(0.75, 16)
+  end
+  def keys
+    all_keys = []
+    @buckets.each do |pair|
+      if !pair.empty?
+         all_keys << pair[0][0]  
+      end 
+    end
+    p all_keys
   end
 end
 
@@ -38,5 +77,13 @@ p hash.set('odin', 533)
 p hash.set('vitor', 533)
 p hash.set('pedro', 533)
 p hash.set('hash', 533)
-p hash.get_value('hash')
+p hash.get('hash')
+# p hash.has?('')
+  hash.remove('hash')
 p hash.buckets
+
+p hash.length
+# p hash.clear
+p hash.buckets
+
+p hash.keys
